20a4fb15df0e3375e8b171399a9340ac1a8c8951
dzambr13 <dayvuhz@gmail.com>
2022-07-28 10:42:40 -0700

    indexjs for controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const bobaController = require('./bobaController')
 const reviewController = require('./reviewController')
 
+// console.log('funnelled both controlled into index')
+
 module.exports = {
   bobaController,
   reviewController
